37c066a0a2a7fab58a57369f4943e4fd5ea09c4b
Jovialngandu <jovialngandu2@gmail.com>
2026-04-24 10:34:17 +0100

    [UPDATE|ADD] Readaptation config

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'], // Ton dossier source
+          alias: {
+            '@store': './src/store',
+            '@services': './src/services',
+            '@components': './src/components',
+            '@features': './src/features',
+            '@db': './src/db',
+          },
+        },
+      ],
+    ],
+  };
+};
