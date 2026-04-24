5d8071f2b2b5adefa89ef88a99e17abf046295c5
Jovialngandu <jovialngandu2@gmail.com>
2026-04-24 22:45:11 +0100

    [ADD]  Ajout de la Root navigator elment

---
+++ src/features/articles/index.js
@@ -0,0 +1,8 @@
+/**
+ * Export articles screens and components
+ */
+export { default as FeedScreen } from './FeedScreen';
+// export { default as ArticleDetailScreen } from './ArticleDetailScreen';
+// export { default as LikedArticlesScreen } from './LikedArticlesScreen';
+export { default as ArticleCard } from './ArticleCard';
+export { default as SearchBar } from './SearchBar';
